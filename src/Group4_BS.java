46335fd584330b62b94915f6840a18d670242678
Michael Konstantinou <mikhailkonstan@hotmail.com>
2022-03-22 11:08:00 +0100

    Updated: Bidding Strategy

--- src/Group4_BS.java
+++ src/Group4_BS.java
@@ -30,6 +30,16 @@ public class Group4_BS extends OfferingStrategy {
      */
     private double scareThreshold;
 
+    /** Sets the threshold regarding when to
+     *  start conceding (eg. 90% of the time)
+     *  Takes values between 0 and 1 and it is only
+     *  enabled during the offensive profile
+     */
+    private double concedeThreshold;
+
+    /** Minimum target utility */
+    private double minUtility;
+
     /** Used to determine whether the agent should be cooperative or not */
     private boolean isOpponentCooperative;
 
@@ -52,6 +62,18 @@ public class Group4_BS extends OfferingStrategy {
             this.scareThreshold = parameters.get("scareThreshold");
         else
             this.scareThreshold = 0.90;
+
+        // Assign parameters to class
+        if (parameters.get("concedeThreshold") != null)
+            this.concedeThreshold = parameters.get("concedeThreshold");
+        else
+            this.concedeThreshold = 0.90;
+
+        // Assign parameters to class
+        if (parameters.get("minUtility") != null)
+            this.minUtility = parameters.get("minUtility");
+        else
+            this.minUtility = 0.50;
 
         this.opponentModel = model;
         this.omStrategy = oms;
@@ -79,29 +101,41 @@ public class Group4_BS extends OfferingStrategy {
         // 2. Check the time if it's close to 90% and apply scare attacks
         // 3. Try to find an offer that supports both users
         double time = negotiationSession.getTime();
-        double utilityGoal = 0.80;
+        double utilityGoal = 1;
 
         // What is the agent's profile?
         if (isOpponentCooperative) {
 
-            // Cooperative Profile
-            // Step 1: Concede little by little every turn
+            /**
+             * Cooperative Profile
+             * The agent concedes with more ease over time as it tries to find
+             * an offer that both users will agree on
+             */
             utilityGoal = p(time);
-
-            // Step 2: Calculate a new utility goal
-
         } else {
 
-            // Offensive Profile
+            /**
+             * Offensive Profile
+             * The agent does not concede over time but concedes only at the end (configurable)
+             * In addition the agent disappears towards the end in order to scare the opponent
+             */
 
             // Step 1: Check whether the agent should scare the opponent
             if (time >= scareThreshold) {
                 System.out.println("Scare opponent!");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
 
             // Step 2: Do not concede unless a configurable amount of time passed
-
-            utilityGoal = 0.80;
+            if (time >= concedeThreshold) {
+                utilityGoal = p(time);
+            } else {
+                utilityGoal = 0.90;
+            }
         }
 
         // if there is no opponent model available
@@ -121,7 +155,8 @@ public class Group4_BS extends OfferingStrategy {
     public Set<BOAparameter> getParameterSpec() {
         Set<BOAparameter> set = new HashSet<BOAparameter>();
         set.add(new BOAparameter("scareThreshold", 0.90, "Scare opponent time threshold"));
-
+        set.add(new BOAparameter("concedeThreshold", 0.90, "Offensive profile concede time threshold"));
+        set.add(new BOAparameter("min", 0.50, "Minimum utility"));
         return set;
     }
 
@@ -133,12 +168,12 @@ public class Group4_BS extends OfferingStrategy {
 
     /**
      * Makes sure the target utility with in the acceptable range according to
-     * the domain. Goes from Pmax to Pmin!
+     * the domain. Goes from 1 to minimum Utility!
      *
      * @param t
      * @return double
      */
     public double p(double t) {
-        return 0.50 + (1 - 0.50) * (1 - t);
+        return minUtility + (1 - minUtility) * (1 -  Math.pow(t, 1.0 / 1.0));
     }
 }
